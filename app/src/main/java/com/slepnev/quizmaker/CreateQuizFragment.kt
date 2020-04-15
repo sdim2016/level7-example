be0b3775369cb8ab87c83b0a53899842a38af23d
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-04-15 17:21:13 +0300

    Implemented create quiz and play quiz

--- app/src/main/java/com/slepnev/quizmaker/CreateQuizFragment.kt
+++ app/src/main/java/com/slepnev/quizmaker/CreateQuizFragment.kt
@@ -6,18 +6,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_create_quiz.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class CreateQuizFragment : Fragment() {
 
+    private lateinit var viewModel: QuizViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Initialize the Shared Activity ViewModel
+        viewModel = ViewModelProvider(activity as AppCompatActivity).get(QuizViewModel::class.java)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_quiz, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnCreate.setOnClickListener {
+            viewModel.createQuestion(
+                etQuestion.text.toString(),
+                etAnswer.text.toString())
+            findNavController().navigateUp()
+        }
     }
 
 
