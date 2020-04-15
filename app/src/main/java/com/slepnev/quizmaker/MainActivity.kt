1a2559d2c2c1997c7fe1409c042c4d7a85c32b22
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-04-15 16:57:44 +0300

    Configured the MainActivity using NavController

--- app/src/main/java/com/slepnev/quizmaker/MainActivity.kt
+++ app/src/main/java/com/slepnev/quizmaker/MainActivity.kt
@@ -2,11 +2,23 @@ package com.slepnev.quizmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
     }
 }
